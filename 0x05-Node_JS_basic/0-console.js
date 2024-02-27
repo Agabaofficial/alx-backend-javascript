916f66c94d4fd1d5b00a8af4437bc46b6f65c339
Agaba Olivier <125471253+Agabaofficial@users.noreply.github.com>
2024-02-27 17:26:50 +0300

    source files

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Agaba Olivier <https://github.com/Agabaofficial>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
